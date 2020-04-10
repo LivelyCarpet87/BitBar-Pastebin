3e434b43196bf002dfe40313c22723f0f59a8b5d
LivelyCarpet87-v2 <58301587+LivelyCarpet87-v2@users.noreply.github.com>
2020-04-11 01:31:17 +0800

    Update pastebin.1m.sh

--- pastebin.1m.sh
+++ pastebin.1m.sh
@@ -7,7 +7,7 @@
 # <bitbar.author.github>LivelyCarpet87</bitbar.author.github>
 # <bitbar.desc>Uses a set of Pastebin API keys to check for pastes created by the user. It will provide links to all the pastes it finds by the user. </bitbar.desc>
 # <bitbar.image>https://i.ibb.co/wJM47wp/pastebin.png</bitbar.image>
-# <bitbar.abouturl></bitbar.abouturl>
+# <bitbar.abouturl>https://github.com/LivelyCarpet87/BitBar-Pastebin#bitbar-pastebin</bitbar.abouturl>
 #
 
 #Requires Configuration Here
